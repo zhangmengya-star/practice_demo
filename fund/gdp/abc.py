1dd230dd50b59fd7d195689dd500caa5a54af177
zhangmengya-star <2406715954@qq.com>
2021-09-11 18:17:50 +0800

    删除无关代码

--- fund/gdp/abc.py
+++
@@ -1,4 +0,0 @@
-class Abc:
-
-    def abc_demo(self):
-        print('abc_demo')
--- fund/gdp/conftest.py
+++
@@ -1,6 +0,0 @@
-def pytest_configure(config):
-    markers_list = ["search", "login"]
-    for markers in markers_list:
-        config.addinivalue_line(
-            "markers", markers
-        )
