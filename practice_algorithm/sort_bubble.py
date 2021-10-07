d31ac0bc3c29015066896a9d0cc1e9d6132275f0
zhangmengya-star <2406715954@qq.com>
2021-10-08 07:27:42 +0800

    代码优化

--- practice_algorithm/sort_bubble.py
+++ practice_algorithm/sort_bubble.py
@@ -10,9 +10,7 @@ def bubble_sort(list):
     for i in range(length - 1):
         for j in range(length - 1 - i):
             if list[j] > list[j + 1]:
-                temp = list[j + 1]
-                list[j + 1] = list[j]
-                list[j] = temp
+                list[j], list[j + 1] = list[j + 1], list[j]
 
     return list
 
