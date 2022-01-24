7262d3b8a4a4b4dc21317c7d7c3b1c4a21d41ab5
zhangmengya-star <2406715954@qq.com>
2022-01-24 19:54:05 +0800

    动态规划-杨辉三角

---
+++ practice_algorithm/generate.py
@@ -0,0 +1,42 @@
+'''
+118. 杨辉三角
+给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+示例 1:
+输入: numRows = 5
+输出: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+示例 2:
+输入: numRows = 1
+输出: [[1]]
+ 
+
+提示:
+1 <= numRows <= 30
+'''
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = [[0] * (i+1) for i in range(numRows)]
+        for i in range(numRows):
+            j = 0
+            while j <= i:
+                if j == 0 or j == i:
+                    result[i][j] = 1
+                else:
+                    result[i][j] = result[i-1][j-1] + result[i-1][j]
+                j += 1
+        return result
+
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = 6
+    result = solution.generate(nums)
+    print(result)
